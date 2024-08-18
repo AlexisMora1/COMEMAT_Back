f5cb9fe0c03d4ebe4e1905921b5d37a8ea5d9c08
AlexisMora1 <ley.noe6@outlook.es>
2024-08-17 23:39:24 -0600

    add : POST to Course API

--- com_back/APICursos/models.py
+++ com_back/APICursos/models.py
@@ -2,14 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Cursos_Page(models.Model):
-    course_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=250)
     created_at = models.DateTimeField()
     image = models.ImageField()
+
+    def __str__(self):
+        return self.name
     
 
 class Curso_info(models.Model):
-    course_id = models.OneToOneField('Cursos_Page', on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Cursos_Page, on_delete=models.CASCADE, related_name='course_id')
     des = models.CharField(max_length=350)
     opinions = models.CharField(max_length=250)
     valuation = models.FloatField()
--- com_back/APICursos/views.py
+++ com_back/APICursos/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from datetime import datetime
+from django.utils import timezone
+from .models import *
 
 # Create your views here.
 
@@ -16,7 +19,60 @@ class Course_List_DB(APIView):
         return Response(data)
     
     def post(self, request):
-        request_data = request.data
-        print('recibimos: ', request_data)
+        try:
+            course = request.data
+            try:
+                # First lets save the page data of the new course
+                cpg = course['page_data']
+                # Calculating the current day
+                created_at_naive = datetime.today()
+                created_at = timezone.make_aware(created_at_naive)
+                # Registering the JSON data in the Cursos_Page table
+                cpg_register = Cursos_Page(
+                                    name = cpg['name'],
+                                    created_at = created_at,
+                                    image = cpg['image']
+                                )
+                # Saving the cpg_register
+                cpg_register.save()
+            except Exception as e: 
+                print(e)
+                return Response({'status':500,"Error":"Error in page_data format"})
+            
+            # Now lets save the Course info in its respective table:
+            try:
+                cinfo = course['course_info']
+                # Registering the JSON data in the Curso_info table
+                cinfo_register = Curso_info(
+                                    course_id = cpg_register,
+                                    des = cinfo['des'],
+                                    opinions = cinfo['opinions'],
+                                    valuation = cinfo['valuation'],
+                                    count_mod = cinfo['count_mod'],
+                                )
+                # Saving the register
+                cinfo_register.save()
 
-        return Response({'data':request_data})
+            except Exception as e:
+                print(e)
+                return Response({'status':500,"Error":"Error in course data format"})
+            
+            # Finally we save the modules info:
+            try:
+                c_modules = course['course_modules']
+                # Registering the JSON data in the Curso_info table
+                register = Modules(module_id = cinfo_register,
+                                    url = c_modules['url'],
+                                )
+                
+                # Saving the register
+                register.save()
+
+            except Exception as e:
+                print(e)
+                return Response({'status':500,"Error":"Error in module data format"})
+
+            return Response({'status':200})
+        
+        except Exception as e:
+            return Response({"status":500,"Error":"Error in POST method"})
