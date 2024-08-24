a8641f10ff97b2f607105cbc70e8172454466fc7
tjonac <tjonathan444@gmail.com>
2024-08-24 08:53:06 -0600

    add doc

--- com_back/com_back/settings.py
+++ com_back/com_back/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'APICursos.apps.ApicursosConfig',
+    'drf_yasg',
     'CoursesAPI.apps.CoursesapiConfig',
     'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
